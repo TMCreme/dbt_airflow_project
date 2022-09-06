6ca2e7b3e87a582c07621a47140090c0fa478589
TMCreme <tonnybright123@yahoo.com>
2022-09-06 22:58:44 +0000

    added custom date handling and pep8 format

--- dags/do_dag.py
+++ dags/do_dag.py
@@ -1,31 +1,25 @@
-import logging 
-from datetime import timedelta, datetime
-from airflow import DAG 
-from airflow.utils.dates import days_ago 
+from datetime import timedelta
+from airflow import DAG
+from airflow.utils.dates import days_ago
+from airflow.operators.python_operator import PythonOperator
 from airflow.operators.bash_operator import BashOperator
-from airflow.operators.python_operator import PythonOperator 
 from airflow.providers.postgres.hooks.postgres import PostgresHook
 from airflow.providers.postgres.operators.postgres import PostgresOperator
-from airflow.providers.amazon.aws.hooks.s3 import S3Hook
 from airflow.sensors.filesystem import FileSensor
 from airflow.models import Variable
-from airflow.providers.amazon.aws.transfers.local_to_s3 import LocalFilesystemToS3Operator
-
-# from airflow.providers.post
-
 from do_read import read_from_s3
 
 
-# Define the default arguments for the DAG 
+# Define the default arguments for the DAG
 default_args = {
-    "owner" : "airflow",
-    "depends_on_past" : False,
-    "start_date" : days_ago(2),
-    "email" : ["admin"],
-    "email_on_failure" : False,
-    "email_on_retry" : False,
-    "retries" : 0,
-    "retry_delay" : timedelta(minutes=5)
+    "owner": "airflow",
+    "depends_on_past": False,
+    "start_date": days_ago(2),
+    "email": ["admin"],
+    "email_on_failure": False,
+    "email_on_retry": False,
+    "retries": 0,
+    "retry_delay": timedelta(minutes=5),
 }
 
 
@@ -33,48 +27,56 @@ dag = DAG(
     "do_pipeline_test",
     default_args=default_args,
     description="Testing the scenarios for DO Pipeline",
-    schedule_interval=timedelta(days=2)
+    schedule_interval=timedelta(days=2),
 )
 
 
 download_file = PythonOperator(
     task_id="download_s3_file",
     python_callable=read_from_s3,
-    op_args=["/tmp/billbox_account_20220713.csv","backups/billbox_account_20220713.json"],
-    dag=dag
+    op_args=[
+        "/tmp/billbox_account_20220713.csv",
+        "backups/billbox_account_20220713.json",
+    ],
+    dag=dag,
 )
 
 local_file_sensor = FileSensor(
-    task_id="sensing_local_file",
-    filepath=Variable.get("do_download_file"),
-    dag=dag
+    task_id="sensing_local_file", filepath=Variable.get("do_download_file"), dag=dag
 )
 
-# create_script_file_sensor = FileSensor(
-#     task_id="sensing_local_file_sql",
-#     filepath="/create_script.sql",
-#     dag=dag
-# )
 
 creating_table = PostgresOperator(
-    task_id = "creating_table_with_column",
+    task_id="creating_table_with_column",
     postgres_conn_id="postgres_default",
     sql=Variable.get("table_create_sql"),
-    dag=dag
+    dag=dag,
 )
+
 
 def copy_into_postgres():
     postgre_hook = PostgresHook(postgres_conn_id="postgres_default")
     postgre_hook.copy_expert(
         """COPY do_billbox.public.billbox_accounts FROM stdin WITH CSV HEADER
             DELIMITER as ',' """,
-            Variable.get("do_download_file")
+        Variable.get("do_download_file"),
     )
 
+
 copy_local_to_postgres = PythonOperator(
-    task_id="copy_from_s3_into_postgres",
-    python_callable=copy_into_postgres,
-    dag=dag
+    task_id="copy_from_s3_into_postgres", python_callable=copy_into_postgres, dag=dag
 )
 
-download_file >> local_file_sensor >> creating_table >> copy_local_to_postgres
+dbt_operator = BashOperator(
+    task_id="dbt_task_in_airflow",
+    bash_command="cd /home/tonny/dteng/airflowproj && dbt run",
+    dag=dag,
+)
+
+(
+    download_file
+    >> local_file_sensor
+    >> creating_table
+    >> copy_local_to_postgres
+    >> dbt_operator
+)
--- dags/do_read.py
+++ dags/do_read.py
@@ -1,8 +1,6 @@
 import pandas as pd
 import boto3
 from airflow.models import Variable
-import configparser
-
 
 
 def read_from_s3(local_file, s3_file):
@@ -19,19 +17,28 @@ def read_from_s3(local_file, s3_file):
     df["_id"] = df["_id"].astype(str).str.lstrip("{'$oid': '")
     df["_id"] = df["_id"].astype(str).str.strip("'}")
     # {'$date': '2015-05-07T16:08:00.201Z'}
+    # Replacing the empty date fields because transforming nan in SQL was a problem
+    df["dateModified"] = df["dateModified"].fillna("")
+    df["dateCreated"] = df["dateCreated"].fillna("")
+
+    # Stripping the date objects to have only the date string
     df["dateCreated"] = df["dateCreated"].astype(str).str.lstrip("{'$date': '")
     df["dateCreated"] = df["dateCreated"].astype(str).str.strip("'}")
+
+    df["dateModified"] = df["dateModified"].astype(str).str.lstrip("{'$date': '")
+    df["dateModified"] = df["dateModified"].astype(str).str.strip("'}")
 
     df.to_csv(local_file, index=False)
     df_header = pd.read_csv(local_file, nrows=0).columns.tolist()
     df_header_edit = [str(item) + " character varying(500)" for item in df_header]
-    
+
     Variable.set("do_download_file", local_file)
+
     creation_script = "".join(str(x) + "," for x in df_header_edit)
     Variable.set(
         "table_create_sql",
-        "CREATE TABLE do_billbox.public.billbox_accounts ( {} )".format(creation_script[:-1]),
+        "CREATE TABLE IF NOT EXISTS do_billbox.public.billbox_accounts ( {} )".format(
+            creation_script[:-1]
+        ),
     )
     return
-
-
